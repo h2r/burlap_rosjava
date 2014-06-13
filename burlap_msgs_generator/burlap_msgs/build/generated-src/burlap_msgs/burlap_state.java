931228d334797999ade668943433ab7adbe3cbc0
stephen <brawner@gmail.com>
2014-06-13 18:26:01 -0400

    removing build files

--- burlap_msgs_generator/burlap_msgs/build/generated-src/burlap_msgs/burlap_state.java
+++
@@ -1,8 +0,0 @@
-package burlap_msgs;
-
-public interface burlap_state extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "burlap_msgs/burlap_state";
-  static final java.lang.String _DEFINITION = "burlap_object[] objects\n";
-  java.util.List<burlap_msgs.burlap_object> getObjects();
-  void setObjects(java.util.List<burlap_msgs.burlap_object> value);
-}
